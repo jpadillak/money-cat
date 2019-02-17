4b37aba80b55495c4efb3b2a77cadc79e719162c
jpadillak <jpadillak@gmail.com>
2019-02-17 17:07:05 -0600

    Setup/gh 2 project skeleton (#4)
    GH-2 Project skeleton

---
+++ Source/MoneyCat.API/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace MoneyCat.API
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			IWebHost webHost = CreateWebHostBuilder(args).ConfigureLogging((logginBuilder) =>
+			{
+				logginBuilder.ClearProviders();
+				logginBuilder.AddDebug();
+				logginBuilder.AddConsole();
+			}).Build();
+			webHost.Run();
+		}
+
+		public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+			WebHost.CreateDefaultBuilder(args)
+				.UseStartup<Startup>();
+	}
+}
