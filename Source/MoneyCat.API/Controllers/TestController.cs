fc7f8cc3cfcb6943c17cea4fe728e6e71f386b1d
Javier Padilla <jpadillak@gmail.com>
2019-02-17 15:16:12 -0600

    GH-2 Moved from vscode to Visual Studio

---
+++ Source/MoneyCat.API/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MoneyCat.API.Controllers
+{
+	[Route("api/test")]
+	public class TestController : Controller
+	{
+		[HttpGet()]
+		public JsonResult GetTest()
+		{
+			return new JsonResult(new List<object>() {
+				new { id=1, name="Hello World"}
+			});
+		}
+	}
+}
